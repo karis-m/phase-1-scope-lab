e07801a86ff728a87093e346baff5b27d4b58527
moses <moses.kamau@student.moringaschool.com>
2023-06-21 12:16:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer = undefined;
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob'; 
+    return bestCustomer;
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+function changeLeastFavoriteCustomer(){
+    throw new Error('Assignment to constant variable.');
+}
